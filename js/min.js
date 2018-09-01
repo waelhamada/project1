d8016f788cbca616f416c37a31a276c7bbc3f431
waelhamada <mr.waelhamada@gmail.com>
2018-09-01 08:45:16 +0200

    member 1

--- js/min.js
+++ js/min.js
@@ -97,3 +97,39 @@ function chooseLarg()
 		alert(num1.value);
 	}
 }
+
+$(document).ready( function ()
+{
+  $('.thumbnile img').on('click',function(){
+  $(this).addClass('selected').siblings().removeClass('selected');
+	  $('.img-content img').attr('src',$(this).attr('src'));
+	 
+  })  
+	 
+  $('.img-content .fa-arrow-circle-right').click(function(){
+	  
+	  if($('.thumbnile .selected').is(':last-child')){
+		  $('.thumbnile img').eq(0).click();
+		  		  
+	  }
+	  else{
+	  $('.thumbnile .selected').next().click();
+	  }
+  })
+	
+	$('.img-content .fa-arrow-circle-left').click(function(){
+		
+		if($('.thumbnile .selected').is(':first-child')){
+		 $('.thumbnile img').eq(2).click();	
+		}
+		else{
+	  
+	  $('.thumbnile .selected').prev().click();
+			}
+  }) 
+	
+	$('.fa-angle-right').click(function(){
+		$(this).toggleClass('fa-angle-right fa-angle-down');
+		
+	})
+});
